3a66ab8c9f3e92f5c34dfaf7f82137aba702576b
Tom White <eugene.white@gmail.com>
2019-10-15 20:07:29 +0000

    making controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    "Hello Moto."
-  end
+      erb :index
+    end
 
+    helpers do
+      def logged_in?
+        !!session[:user_id]
+      end
+
+      def current_user
+        User.find(session[:user_id])
+      end
+    end
 end
