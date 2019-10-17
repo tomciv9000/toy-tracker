7461e88680314b491c9425ff60e0f979521aa0b0
Tom White <eugene.white@gmail.com>
2019-10-17 02:47:07 +0000

    testing version of post /toys route

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -9,4 +9,16 @@ class ToysController < ApplicationController
     end
   end
 
+  post '/toys' do
+    binding.pry
+    @toy = Toy.create(params[:toy])
+    if !params["manufacturer"]["name"].empty?
+      @toy.manufacturer = Manufacturer.create(name: params["manufacturer"]["name"])
+      # When using the shovel operator, ActiveRecord instantly fires update SQL
+      # without waiting for the save or update call on the parent object,
+      # unless the parent object is a new record.
+    end
+    redirect "/toys/#{@toy.id}"
+  end
+
 end
