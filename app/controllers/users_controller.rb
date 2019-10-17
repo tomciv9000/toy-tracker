8bcc256a1129ba2b931d2cb1513fb9ac20c2f127
Tom White <eugene.white@gmail.com>
2019-10-17 01:33:52 +0000

    fixed another kids routing issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
      @user.save
      session[:user_id] = @user.id
-     redirect to '/kids/index'
+     redirect to '/kids'
    end
   end
 
