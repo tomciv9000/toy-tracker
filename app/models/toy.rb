e40f0be9e456fe904ac70b980edac22cf08c1490
Tom White <eugene.white@gmail.com>
2019-10-23 05:02:20 +0000

    private category view working!

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -5,14 +5,4 @@ class Toy < ActiveRecord::Base
   has_many :toy_categories
   has_many :categories, :through => :toy_categories
 
-  # def slug
-  #   name.downcase.gsub(" ","-")
-  # end
-  Book.joins(:genres).where("genres.name" => ["Young Adult", "Fiction", "Romance"])
-  Book.joins(:genres).where("genres.name" => "Young Adult")
-  #
-  #
-  # def self.find_by_slug(slug)
-  #   Toy.all.find{|toy| toy.slug == slug}
-  # end
  end
