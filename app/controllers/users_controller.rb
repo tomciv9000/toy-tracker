c8bd095d27e274baf076575f1f8866cee4707e05
Tom White <eugene.white@gmail.com>
2019-10-24 19:08:59 +0000

    moved User validation to model, removed logic from controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,18 @@ use Rack::Flash
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-     redirect to '/signup'
-   else
-     @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    binding.pry
+    @user = User.new(params)
+    if !@user.valid?
+      redirect to '/signup'
+    else
      @user.save
      session[:user_id] = @user.id
      redirect to '/kids'
-   end
+    end
   end
 
-  get "/login" do
+  get '/login' do
     if !logged_in?
       erb :'/users/login'
     else
@@ -29,7 +30,7 @@ use Rack::Flash
     end
   end
 
-  post "/login" do
+  post '/login' do
     user = User.find_by(:username => params[:username])
 		if user && user.authenticate(params[:password])
 		  session[:user_id] = user.id
@@ -39,21 +40,21 @@ use Rack::Flash
 		end
   end
 
-  get "/logout" do
+  get '/logout' do
     if logged_in?
       session.clear
-      redirect "/login"
+      redirect '/login'
     else
-      redirect "/login"
+      redirect '/login'
     end
   end
 
-  get "/users/:id" do
+  get '/users/:id' do
     if logged_in? && current_user.id == params[:id].to_i
       @user = User.find(params[:id])
       erb :'users/show'
     else
-      redirect "/login"
+      redirect '/login'
     end
   end
 
