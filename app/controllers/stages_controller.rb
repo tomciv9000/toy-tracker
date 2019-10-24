e7e9ac9c5f569e6f6c93d899fc9517979b9bf6b2
Tom White <eugene.white@gmail.com>
2019-10-24 02:56:25 +0000

    working stage and category controllers/views

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -12,21 +12,13 @@ class StagesController < ApplicationController
   get "/stages/:id" do
     if logged_in?
       @stage = Stage.find(params[:id])
-      erb :'stages/show'
-    else
-    redirect to '/login'
-    end
-  end
-
-  get "/stages/:id" do
-    if logged_in?
-      @stage = Stage.find(params[:id])
       @user_kids_by_stage = []
       @user_toys_by_stage = []
       current_user.kids.each do |kid|
         if kid.stage = @stage
           @user_kids_by_stage << kid
         end
+      end
       current_user.toys.each do |toy|
         if toy.stage = @stage
           @user_toys_by_stage << toy
