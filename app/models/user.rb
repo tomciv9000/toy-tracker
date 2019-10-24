5ba28475c9d62cbc7d4531c1bdb690f32bad241a
Tom White <eugene.white@gmail.com>
2019-10-24 03:39:59 +0000

    validates_uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :kids
   has_many :toys, :through => :kids
+  validates_uniqueness_of :username
 
-  def slug
-    username.downcase.gsub(" ","-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
-  end
 
 end
