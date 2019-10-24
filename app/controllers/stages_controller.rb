3099ecf4dc7c978089494525ea1168a93e3d54f3
Tom White <eugene.white@gmail.com>
2019-10-24 02:21:58 +0000

    user kids by stage and user toys by stage

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -18,4 +18,24 @@ class StagesController < ApplicationController
     end
   end
 
+  get "/stages/:id" do
+    if logged_in?
+      @stage = Stage.find(params[:id])
+      @user_kids_by_stage = []
+      @user_toys_by_stage = []
+      current_user.kids.each do |kid|
+        if kid.stage = @stage
+          @user_kids_by_stage << kid
+        end
+      current_user.toys.each do |toy|
+        if toy.stage = @stage
+          @user_toys_by_stage << toy
+        end
+      end
+      erb :'stages/show'
+    else
+    redirect to '/login'
+    end
+  end
+
 end
