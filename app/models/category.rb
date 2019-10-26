09b3e8cfea9aa0d16cc1170c9b718c7779232b29
Tom White <eugene.white@gmail.com>
2019-10-26 03:08:12 +0000

    formatting, typos

--- app/models/category.rb
+++ app/models/category.rb
@@ -29,11 +29,4 @@ class Category < ActiveRecord::Base
     @list_categories = Category.all
   end
 
-  # def slug
-  #   name.downcase.gsub(" ","-")
-  # end
-  #
-  # def self.find_by_slug(slug)
-  #   Category.all.find{|category| type.slug == slug}
-  # end
  end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -4,13 +4,4 @@ class Kid < ActiveRecord::Base
   has_many :toys
   validates :name, :stage, :user, presence: true
 
-### these methods were an attempt to create a slug that allowed for duplicate names
-  # def slug_id
-  #    name.downcase.gsub(" ","-") << "_#{id}"
-  # end
-  #
-  # def self.find_by_slug_id(slug_id)
-  #    Kid.all.find{|kid| kid.slug_id == slug_id}
-  # end
-
 end
