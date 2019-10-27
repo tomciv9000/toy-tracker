f6cc320717cc3b2a0199256958bb04257c0db934
Tom White <eugene.white@gmail.com>
2019-10-27 23:55:46 +0000

    fixed stage error

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -14,12 +14,12 @@ use Rack::Flash
       @user_kids_by_stage = []
       @user_toys_by_stage = []
       current_user.kids.each do |kid|
-        if kid.stage = @stage
+        if kid.stage == @stage
           @user_kids_by_stage << kid
         end
       end
       current_user.toys.each do |toy|
-        if toy.stage = @stage
+        if toy.stage == @stage
           @user_toys_by_stage << toy
         end
       end
