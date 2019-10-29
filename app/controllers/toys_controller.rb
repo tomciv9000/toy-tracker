c3ffc75d2b191a459c98bc3ab735f683adef8990
Tom White <eugene.white@gmail.com>
2019-10-29 15:56:05 +0000

    redirect if not owned helper method

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -27,33 +27,25 @@ class ToysController < ApplicationController
 
   get '/toys/:id' do
     redirect_if_not_logged_in
-    @toy = Toy.find(params[:id])
-    if current_user.toys.include?(@toy)
-      erb :'toys/show'
-    else
-      flash[:errors] = "Can't find that toy!"
-      redirect to '/toys'
-    end
+    @toy = Toy.find_by_id(params[:id])
+    redirect_if_not_user_owned(@toy)
+    erb :'toys/show'
   end
 
   get '/toys/:id/edit' do
     redirect_if_not_logged_in
     @categories = Category.list_categories
     @stages = Stage.development_stages
-    @toy = Toy.find(params[:id])
-    if current_user.toys.include?(@toy)
-      erb :'toys/edit'
-    else
-      flash[:errors] = "Can't find that toy!"
-      redirect to '/toys'
-    end
+    @toy = Toy.find_by_id(params[:id])
+    redirect_if_not_user_owned(@toy)
+    erb :'toys/edit'
   end
 
   patch '/toys/:id' do
     redirect_if_not_logged_in
-    @toy = Toy.find(params[:id])
-    if current_user.toys.include?(@toy)
-      @toy.assign_attributes(params[:toy])
+    @toy = Toy.find_by_id(params[:id])
+    redirect_if_not_user_owned(@toy)
+    @toy.assign_attributes(params[:toy])
       if !@toy.valid?
         flash[:errors] = @toy.errors.full_messages
         redirect to "/toys/#{@toy.id}"
@@ -62,23 +54,27 @@ class ToysController < ApplicationController
         flash[:message] = "Successfully updated toy"
         redirect to "/toys/#{@toy.id}"
       end
-    else
-      flash[:message] = "You do not have access to that toy."
-      redirect to "/toys"
-    end
   end
 
   delete '/toys/:id/delete' do
     redirect_if_not_logged_in
     @toy = Toy.find_by_id(params[:id])
-    if current_user.toys.include?(@toy)
-      @toy.delete
-      flash[:message] = "Successfully deleted toy"
-      redirect to "/toys"
-    else
-      flash[:message] = "You do not have access to that toy."
-      redirect to "/toys"
-    end
+    redirect_if_not_user_owned(@toy)
+    @toy.delete
+    flash[:message] = "Successfully deleted toy"
+    redirect to "/toys"
   end
+
+  helpers do
+
+    def redirect_if_not_user_owned(toy)
+      if !current_user.toys.include?(toy)
+        flash[:message] = "Toy not found!"
+        redirect to "/toys"
+      end
+    end
+
+  end
+
 
 end
