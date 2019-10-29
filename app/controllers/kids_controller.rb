71c8315cc3ab2da8bc44e7a0a401e6282ab10bd0
Tom White <eugene.white@gmail.com>
2019-10-29 16:03:36 +0000

    redirect if not users kid helper

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -25,55 +25,50 @@ class KidsController < ApplicationController
 
   get "/kids/:id" do
     redirect_if_not_logged_in
-    @kid = Kid.find(params[:id])
-    if current_user.kids.include?(@kid)
-      erb :'kids/show'
-    else
-      flash[:errors] = "Not one of yours!"
-      redirect to '/kids'
-    end
+    @kid = Kid.find_by_id(params[:id])
+    redirect_if_not_users_kid(@kid)
+    erb :'kids/show'
   end
 
   get '/kids/:id/edit' do
     redirect_if_not_logged_in
-    @kid = Kid.find(params[:id])
-    if current_user.kids.include?(@kid)
-      erb :'kids/edit'
-    else
-      flash[:errors] = "Not one of yours!"
-      redirect to '/kids'
-    end
+    @kid = Kid.find_by_id(params[:id])
+    redirect_if_not_users_kid(@kid)
+    erb :'kids/edit'
   end
 
   patch '/kids/:id' do
     redirect_if_not_logged_in
-    @kid = Kid.find(params[:id])
-    if current_user.kids.include?(@kid)
-      @kid.assign_attributes(params[:kid])
+    @kid = Kid.find_by_id(params[:id])
+    redirect_if_not_users_kid(@kid)
+    @kid.assign_attributes(params[:kid])
       if !@kid.valid?
         flash[:errors] = @kid.errors.full_messages
         redirect to "/kids/#{@kid.id}"
       else
-        @kid .save
+        @kid.save
         flash[:message] = "Successfully updated kid"
         redirect to "/kids/#{@kid.id}"
       end
-    else
-      flash[:message] = "Not one of yours!"
-      redirect to "/kids"
-    end
   end
 
   delete '/kids/:id/delete' do
     redirect_if_not_logged_in
     @kid = Kid.find_by_id(params[:id])
-    if current_user.kids.include?(@kid)
-      @kid.delete
-      flash[:message] = "Successfully deleted kid"
-    else
-      flash[:message] = "Not one of yours!"
-      redirect to '/kids'
+    redirect_if_not_users_kid(@kid)
+    @kid.delete
+    flash[:message] = "Successfully deleted kid"
+  end
+
+  helpers do
+
+    def redirect_if_not_users_kid(kid)
+      if !current_user.kids.include?(kid)
+        flash[:message] = "Kid not found!"
+        redirect to "/kids"
+      end
     end
+
   end
 
 end
